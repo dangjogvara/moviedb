ed249e27e1126ead08b6cbf9fbe7073dda338966
Dan Poulsen <dangjogvara@gmail.com>
2022-06-16 21:30:11 +0100

    fetch movie data onMount

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/popular?api_key=dff69ae49c24386e306a3ba06a0994c9&language=en-US&page=1`
+		);
+		const data = await res.json();
+		console.log(data);
+		if (res.ok) {
+			return {
+				props: { popular: data.results }
+			};
+		}
+	}
+</script>
+
+<script>
+	export let popular;
+</script>
+
+<!-- API KEY dff69ae49c24386e306a3ba06a0994c9 -->
