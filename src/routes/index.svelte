34d2c352d38ec1773d9754fbd6bf7522dac51e26
Dan Poulsen <dangjogvara@gmail.com>
2022-06-20 21:03:14 +0100

    eddsf

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,6 @@
 			`https://api.themoviedb.org/3/movie/popular?api_key=dff69ae49c24386e306a3ba06a0994c9&language=en-US&page=1`
 		);
 		const data = await res.json();
-		console.log(data);
 		if (res.ok) {
 			return {
 				props: { popular: data.results }
@@ -14,7 +13,13 @@
 </script>
 
 <script>
+	import PopularMovies from '../components/PopularMovies.svelte';
+
 	export let popular;
 </script>
 
 <!-- API KEY dff69ae49c24386e306a3ba06a0994c9 -->
+
+<section>
+	<PopularMovies {popular} />
+</section>
